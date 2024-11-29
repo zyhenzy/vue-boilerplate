7479214b3beff6728787a124b3cc7e996db0105f
ying.zhang_zhang <ying.zhang_zhang@neusoft.com>
2024-11-29 17:45:46 +0800

    feat: 引入antd

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/reset.css';
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +12,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Antd)
 
 app.mount('#app')
